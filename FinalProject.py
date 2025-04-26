9892eaa7746696b56ac7591b5101ed6d55aafa40
abhikkonar05 <143819192+abhikkonar05@users.noreply.github.com>
2025-04-26 12:54:18 +0530

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
+import numpy as np
 import calendar
 data=pd.read_csv("D:/Customer Loyalty History.csv")
 print(data)
